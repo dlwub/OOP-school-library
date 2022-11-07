d3c9cbf5d76420656ca0bdc7a0b04508bf6c193d
dlwub <dlwubi@gmail.com>
2022-11-07 18:48:23 +0300

    Create Teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher < Person
+  def initialize(specialization, name = "Unknown", age)
+    super(specialization, name = "Unknown", age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
